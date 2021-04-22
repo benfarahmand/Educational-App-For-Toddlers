fafcd1aeb3d160ec2d6be3c3cb50519f0deabccc
benfarahmand <ben@tuvalabs.com>
2021-04-22 13:05:33 -0400

    added letter sounds
    now when someone clicks on a letter, it will also say the letter

--- toddler.js
+++ toddler.js
@@ -2,8 +2,12 @@ var animalImages;
 var animalCalls;
 var animalNames;
 var animalText;
+var letterSounds;
 var buttons;
+var textfontsize = 78;
 var whileDragging = false;
+var speakerWidth = 70;
+var speakerHeight = 70;
 var imageSize = 200;
 var dir = "./content/animals/";
 var speakImage;
@@ -18,7 +22,8 @@ function setup() {
 	positionButtons();
 	createCanvas(windowWidth, windowHeight-10);
 	background(0);
-	textSize(62);
+	textSize(textfontsize);
+	textAlign(LEFT,TOP)
 	fill(255);
 }
 
@@ -30,14 +35,11 @@ function draw() {
 }
 
 function mouseDragged(){
-	whileDragging = true;
-	for(var i = 0 ; i < buttons.length ; i++){
-		// buttons[i].x=buttons[i].x+(mouseX-pmouseX);
-		buttons[i].y=buttons[i].y+(mouseY-pmouseY);
-		// if(buttons[0].y>-imageSize*buttons.length && buttons[0].y<1){
-		// 	buttons[i].y=buttons[i].y+(mouseY-pmouseY);
-		// } else if(buttons[0].y>=1) buttons[0].y=0;
-		// else if(buttons[0].y<=-imageSize*buttons.length) buttons[0].y=-imageSize*buttons.length+1;
+	if(mouseX<imageSize){
+		whileDragging = true;
+		for(var i = 0 ; i < buttons.length ; i++){
+			buttons[i].y=buttons[i].y+(mouseY-pmouseY);
+		}
 	}
 }
 
@@ -56,7 +58,17 @@ function windowResized() {
 }
 
 function loadFileNames(){
+	print("started loading");
+	loadStrings("./content/lettersounds.txt",loadLetterSounds)
 	loadStrings(dir+"filenames.txt",loadAnimals);
+	print("finished loading");
+}
+
+function loadLetterSounds(result){
+	letterSounds=[];
+	for(var i = 0 ; i < result.length ; i++){
+		letterSounds.push(loadSound("./content/alphabet/"+result[i]));
+	}
 }
 
 function loadAnimals(result){
@@ -64,16 +76,12 @@ function loadAnimals(result){
 	animalNames = [];
 	animalImages = [];
 	animalText = [];
-	
-	print("started loading");
 	for( var i = 0 ; i < result.length ; i+=3){
 		animalCalls.push(loadSound(dir+result[i]));
 		animalNames.push(loadSound(dir+result[i+1]));
 		animalImages.push(loadImage(dir+result[i+2]));
-		animalText.push(split(result[i+2],".")[0].replace("_"," ").toUpperCase());
-		print(Math.trunc(i / result.length*100.0));
+		animalText.push(split(result[i+2],".")[0].replace(/_/g," ").toUpperCase());
 	}
-	print("finished loading");
 }
 
 function createButtons(){
@@ -91,6 +99,78 @@ function positionButtons(){
 	}
 }
 
+class Letter {
+
+	constructor(_c,_x,_y,_w,_h,_soundArrayRef){
+		this.c=_c;
+		this.x = _x;
+		this.y = _y;
+		this.width = _w;
+		this.height = _h;
+		this.soundArrayRef=_soundArrayRef;
+	}
+
+	over() {
+	    if (mouseX > this.x && mouseX < this.x + this.width && mouseY > this.y && mouseY < this.y + this.height) {
+	      return true;
+	    } else {
+	      return false;
+	    }
+	}
+
+	mouseReleased(){
+		if(this.over()) {
+			this.playSound();
+		}
+	}
+
+	playSound(){
+		if(!letterSounds[this.soundArrayRef].isPlaying()) letterSounds[this.soundArrayRef].play();
+	}
+
+}
+
+class Letters {
+
+	constructor(_w,_x,_y){
+		this.word = _w;
+		this.characters = [];
+		this.count = _w.length;
+		this.x = _x;
+	    this.y = _y;
+	    textSize(textfontsize);
+	    this.width = textWidth(_w);
+	    this.height = textfontsize;
+	    var xLoc = _x;
+	    for(var i = 0 ; i < this.count ; i ++){
+	    	var l = this.word.charAt(i);
+	    	var letterWidth = textWidth(l);
+	    	this.characters.push(new Letter(l,xLoc,this.y,letterWidth,textfontsize,alphabetPosition(l)-1));
+	    	xLoc = xLoc + letterWidth;
+	    }
+	}
+
+	over() {
+	    if (mouseX > this.x && mouseX < this.x + this.width && mouseY > this.y && mouseY < this.y + this.height) {
+	      return true;
+	    } else {
+	      return false;
+	    }
+	}
+
+	display(){
+		for(var i = 0 ; i < this.characters.length ; i++){
+			text(this.characters[i].c,this.characters[i].x,this.characters[i].y);
+		}
+	}
+
+	mouseReleased(){
+		for(var i = 0 ; i < this.characters.length ; i++){
+			this.characters[i].mouseReleased();
+		}
+	}
+}
+
 class Button {
   
   constructor(inX, inY, w, h, inImg, myCall, myName, myText, myActive) {
@@ -99,24 +179,27 @@ class Button {
     this.width = w;
     this.height = h;
     this.img = inImg;
+    this.imgX = imageSize+10;
+    this.imgY = 100;
+    this.imgWidth = windowWidth*.5;
     this.call = myCall;
     this.animalName = myName;
-    this.animalText = myText;
+    this.animalText = new Letters(myText,imageSize+speakerWidth+40,20);
     this.active = myActive;
     this.speakX = imageSize+20;
-    this.speakY = 31;
-    this.speakWidth = 50;
-    this.speakHeight = 50;
+    this.speakY = 21;
+    this.speakWidth = speakerWidth;
+    this.speakHeight = speakerHeight;
   }
   
   display() {
     image(this.img, this.x, this.y, this.width, this.height);
     if(this.active){
-    	var w = windowWidth*.5;
-    	if(w<300)w=300;
-    	image(this.img, imageSize+10, 100, w, w);
+    	if(this.imgWidth<300)this.imgWidth=300;
+    	if(this.imgWidth>windowHeight-200)this.imgWidth=windowHeight-200;
+    	image(this.img, this.imgX, this.imgY, this.imgWidth, this.imgWidth);
     	image(speakImage, this.speakX, this.speakY, this.speakWidth, this.speakHeight);
-    	text(this.animalText,imageSize+80,80);
+    	this.animalText.display();
     }
   }
   
@@ -137,6 +220,15 @@ class Button {
     }	
   }
 
+  overLargeImage(){
+  	if (mouseX > this.imgX && mouseX < this.imgX + this.imgWidth && 
+  		mouseY > this.imgY && mouseY < this.imgY + this.imgWidth) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   playCallSound(){
   	if(!this.call.isPlaying()) this.call.play();
   }
@@ -154,9 +246,21 @@ class Button {
 	  		this.active = false;
 	  	}
   	} else {
-  		if(this.overSpeaker() && this.active){
-  			this.playNameSound();
+  		if(this.active){
+	  		if(this.overSpeaker()) this.playNameSound();
+	  		else if(this.overLargeImage()) this.playCallSound();
+	  		else this.animalText.mouseReleased();
   		}
   	}
   }
+}
+
+function alphabetPosition(text) {
+  var result = "";
+  for (var i = 0; i < text.length; i++) {
+    var code = text.toUpperCase().charCodeAt(i)
+    if (code > 64 && code < 91) result += (code - 64) + " ";
+  }
+
+  return result.slice(0, result.length - 1);
 }
